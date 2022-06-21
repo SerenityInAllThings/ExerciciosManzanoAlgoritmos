19a3130d554e066d896f482a5bdb03d77c9a2d8d
vincenzo-bbosco <vincenzo.bbosco@gmail.com>
2022-06-20 21:14:29 -0300

    correção ex 3.10 A

--- lista/3.10/1-L02A.js
+++ lista/3.10/1-L02A.js
@@ -3,8 +3,8 @@ const multiplicador = (A, B, C, D) => {
     try {
         if (isNaN(A) || isNaN(B) || isNaN(C) || isNaN(D)) throw 'Por favor digite um número e não um texto'
 
-        P = A * C
-        S = B + D
+        let P = A * C
+        let S = B + D
 
         console.log('a operação de soma é', S, 'e a de multiplicar é', P)
 
